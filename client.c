699a8853bbf12803e6ddb87da5a1a0ce215aad55
israel <ismalakazel@gmail.com>
2021-06-26 17:51:42 -0300

    Fix code indentation

--- client.c
+++ client.c
@@ -13,102 +13,102 @@
 
 /// Holds data (void *args) that is sent to accept_connections(void *args)
 struct data {
-	int client;
-	char *username;
+  int client;
+  char *username;
 };
- 
+
 /// Writes to a socket.
 /// - Parameter args: A struct data.
 static void *write_to_socket(void *args) {	
-	struct data *data = (struct data *) args;
-	char message[1024];
-	while (fgets(message, sizeof(message), stdin)) {
-		int length = strlen(data->username) + strlen(message) + 2;
-		char username[length];
-		strcpy(username, data->username);
-		strcat(username, ": ");
-		strcat(username, message);
-		ssize_t write_status = write(data->client, (const char *) &username, strlen(username));
-		if (write_status == -1) {
-			handle_error("Write");
-		}
-	};
+  struct data *data = (struct data *) args;
+  char message[1024];
+  while (fgets(message, sizeof(message), stdin)) {
+    int length = strlen(data->username) + strlen(message) + 2;
+    char username[length];
+    strcpy(username, data->username);
+    strcat(username, ": ");
+    strcat(username, message);
+    ssize_t write_status = write(data->client, (const char *) &username, strlen(username));
+    if (write_status == -1) {
+      handle_error("Write");
+    }
+  };
 };
 
 int main(int argc, char * args[]) {
-	
-	if (argc <= 0) {
-		printf("Please provide a username when starting this socket:");
-		exit(EXIT_FAILURE);
-	};	
 
-	char *username = args[1];
+  if (argc <= 0) {
+    printf("Please provide a username when starting this socket:");
+    exit(EXIT_FAILURE);
+  };	
 
-	/// Get socket description.
-	int client = socket(AF_UNIX, SOCK_STREAM, PF_UNIX);
-	struct sockaddr_un addr = { 
-			AF_UNIX,
-			SOCKET_PATH,
-	};
+  char *username = args[1];
 
-	/// Connect to a socket.
-	int socket_conn_fd = connect(client, (struct sockaddr *) &addr, sizeof(addr));
+  /// Get socket description.
+  int client = socket(AF_UNIX, SOCK_STREAM, PF_UNIX);
+  struct sockaddr_un addr = { 
+    AF_UNIX,
+    SOCKET_PATH,
+  };
 
-	/// Wait for user input and write it to the socket.
-	pthread_t tid = 0;
-	struct data *data = malloc(sizeof(struct data));
-	data->client = client;
-	data->username = username;
-	thread(tid, &write_to_socket, (void *) &data);	
+  /// Connect to a socket.
+  int socket_conn_fd = connect(client, (struct sockaddr *) &addr, sizeof(addr));
 
-	/// Socket to be monitored for events.
-	struct pollfd pfd;
-	pfd.fd = client;
-	pfd.events = POLLIN;
-	struct pollfd pollfds[1] = { pfd };	
+  /// Wait for user input and write it to the socket.
+  pthread_t tid = 0;
+  struct data *data = malloc(sizeof(struct data));
+  data->client = client;
+  data->username = username;
+  thread(tid, &write_to_socket, (void *) &data);	
 
-	/// Start polling for socket events.
-	while(1) {
-		const int POLL_RESULT_ERROR = -1;
-		const int POLL_RESULT_TIMEOUT = 0;
-		const int POLL_RESULT_SUCCESS = 1;
-		const int POLL_TIMEOUT = 0.05 * 60 * 1000;
-		int rpoll = poll(pollfds, 1, POLL_TIMEOUT);
-		
-		if (rpoll == POLL_RESULT_ERROR) {
-			handle_error("Poll");
-		};
+  /// Socket to be monitored for events.
+  struct pollfd pfd;
+  pfd.fd = client;
+  pfd.events = POLLIN;
+  struct pollfd pollfds[1] = { pfd };	
 
-		if (rpoll == POLL_RESULT_TIMEOUT) {
-			continue;	
-		};
-	 	
-		/// Client socket has events available. 
-		if (rpoll == POLL_RESULT_SUCCESS) {
-			
-			if (pollfds[0].revents == 0) {
-				continue;			
-			};
+  /// Start polling for socket events.
+  while(1) {
+    const int POLL_RESULT_ERROR = -1;
+    const int POLL_RESULT_TIMEOUT = 0;
+    const int POLL_RESULT_SUCCESS = 1;
+    const int POLL_TIMEOUT = 0.05 * 60 * 1000;
+    int rpoll = poll(pollfds, 1, POLL_TIMEOUT);
 
-			if (pollfds[0].revents != POLLIN) {
-				break;
-			};	
-		
-			/// Read data from socket.
-			int length = 1024;	
-			char buffer[length];
-			int message = read(pfd.fd, buffer, sizeof(buffer)-1);
-			buffer[message] = 0x00;
-			printf("%s", buffer);
-			fflush(stdout);
-			memset(buffer, 0, length);
+    if (rpoll == POLL_RESULT_ERROR) {
+      handle_error("Poll");
+    };
 
-			continue;
-		};	
-	};
+    if (rpoll == POLL_RESULT_TIMEOUT) {
+      continue;	
+    };
 
-	free(data);
-	remove(SOCKET_PATH);
-	close(client);
-	exit(EXIT_SUCCESS);
+    /// Client socket has events available. 
+    if (rpoll == POLL_RESULT_SUCCESS) {
+
+      if (pollfds[0].revents == 0) {
+        continue;			
+      };
+
+      if (pollfds[0].revents != POLLIN) {
+        break;
+      };	
+
+      /// Read data from socket.
+      int length = 1024;	
+      char buffer[length];
+      int message = read(pfd.fd, buffer, sizeof(buffer)-1);
+      buffer[message] = 0x00;
+      printf("%s", buffer);
+      fflush(stdout);
+      memset(buffer, 0, length);
+
+      continue;
+    };	
+  };
+
+  free(data);
+  remove(SOCKET_PATH);
+  close(client);
+  exit(EXIT_SUCCESS);
 }
--- server.c
+++ server.c
@@ -13,138 +13,138 @@
 
 /// Holds data (void *args) that is sent to accept_connections(void *args)
 struct data {
-	int server;
-	int nconnections;
-	struct pollfd *connections;
+  int server;
+  int nconnections;
+  struct pollfd *connections;
 };
 
 /// Createas a new socket (server). For more datarmation see `$ man socket`.
 /// - Returns: The socket file descriptor.
 int create_socket() {
-	int server = socket(AF_UNIX, SOCK_STREAM, PF_UNIX);
-	
-	struct sockaddr_un addr = {
-		AF_UNIX,
-		SOCKET_PATH
-	};
+  int server = socket(AF_UNIX, SOCK_STREAM, PF_UNIX);
 
-	bind(server, (struct sockaddr *) &addr, sizeof(addr));
-	listen(server, SOCKET_BACKLOG);
-	return server;
+  struct sockaddr_un addr = {
+    AF_UNIX,
+    SOCKET_PATH
+  };
+
+  bind(server, (struct sockaddr *) &addr, sizeof(addr));
+  listen(server, SOCKET_BACKLOG);
+  return server;
 };
 
 /// Accepts connections to the server socket.
 /// - Parameter *args: struct data.
 static void *accept_connections(void *args) {	
-	struct data *data = args;
-	while(1) {
-		int fd = accept(data->server, (struct sockaddr *) NULL, NULL); 
-		if (fd == -1) {
-			handle_error("Accept");
-		};
-		
-		struct pollfd connection;
-		connection.fd = fd;
-		connection.events = POLLIN;
-		
-		data->connections = realloc(data->connections, (data->nconnections+1) * sizeof(struct pollfd));
-		data->connections[data->nconnections] = connection;
-		data->nconnections += 1;
-	};
+  struct data *data = args;
+  while(1) {
+    int fd = accept(data->server, (struct sockaddr *) NULL, NULL); 
+    if (fd == -1) {
+      handle_error("Accept");
+    };
+
+    struct pollfd connection;
+    connection.fd = fd;
+    connection.events = POLLIN;
+
+    data->connections = realloc(data->connections, (data->nconnections+1) * sizeof(struct pollfd));
+    data->connections[data->nconnections] = connection;
+    data->nconnections += 1;
+  };
 };
 
 ///	Reads from the connected socket.
 /// - Parameter fd: The connected socket file descriptor.
 /// - Parameter buffer: The buffer to place the bytes in.
 void socket_read(int fd, char **buffer) {
-	int bytes = read(fd, *buffer, sizeof(*buffer));
+  int bytes = read(fd, *buffer, sizeof(*buffer));
 };
 
 /// Writes to a connected socket.
 /// - Parameter fd: The connected socket file descriptor.
 /// - Parameter *message: A pointer to the message buffer.
 void socket_write(int fd, char *message) {
-	ssize_t write_status = write(fd, (const char *) message, sizeof(message));
+  ssize_t write_status = write(fd, (const char *) message, sizeof(message));
 };
 
 int main() {
-	
-	int server = create_socket();
-	
-	/// Accepted socket connections in a separate thread.
-	struct data *data = malloc(sizeof(struct data));
-	data->server = server;
-	data->nconnections = 0;
-	data->connections = malloc(sizeof(struct pollfd));
-	pthread_t thread_id = 0;
-	thread(thread_id,  &accept_connections, (void *) &data);	
 
-	/// Start polling for events on socket connections.
-	while(1) {
-		const int POLL_RESULT_ERROR = -1;
-		const int POLL_RESULT_TIMEOUT = 0;
-		const int POLL_RESULT_SUCCESS = 1;
-		const int POLL_TIMEOUT = 0.05 * 60 * 1000;
-		int rpoll = poll(data->connections, data->nconnections, POLL_TIMEOUT);
-		
-		if (rpoll == POLL_RESULT_ERROR) {
-			handle_error("Poll");
-		};
+  int server = create_socket();
 
-		/// Restart loop after timeout. 
-		if (rpoll == POLL_RESULT_TIMEOUT) {
-			continue;	
-		};
-		
-		/// One or more connections have events available. 
-		if (rpoll == POLL_RESULT_SUCCESS) {
-			for (int i = 0; i < data->nconnections; i++) {
-				
-				struct pollfd connection = data->connections[i];
-			
-				/// No events available.	
-				if (connection.revents == 0) {
-					continue;			
-				};
+  /// Accepted socket connections in a separate thread.
+  struct data *data = malloc(sizeof(struct data));
+  data->server = server;
+  data->nconnections = 0;
+  data->connections = malloc(sizeof(struct pollfd));
+  pthread_t thread_id = 0;
+  thread(thread_id,  &accept_connections, (void *) &data);	
 
-				/// Probably, client connection terminated.
-				if (connection.revents != POLLIN) {					
-					/// Close connection.
-					close(connection.fd);
+  /// Start polling for events on socket connections.
+  while(1) {
+    const int POLL_RESULT_ERROR = -1;
+    const int POLL_RESULT_TIMEOUT = 0;
+    const int POLL_RESULT_SUCCESS = 1;
+    const int POLL_TIMEOUT = 0.05 * 60 * 1000;
+    int rpoll = poll(data->connections, data->nconnections, POLL_TIMEOUT);
 
-					/// Create new connection array without closed connection.
-					int _nconnections = data->nconnections - 1;
-					for (int a = i; a < _nconnections; a++) {
-						data->connections[a] = data->connections[a+1];
-					};
+    if (rpoll == POLL_RESULT_ERROR) {
+      handle_error("Poll");
+    };
 
-					/// Replace old connection array with resized one.
-					data->connections = realloc(data->connections, (_nconnections + 1) * sizeof(struct pollfd));
-					data->nconnections = _nconnections;
-					
-					continue;
-				};
+    /// Restart loop after timeout. 
+    if (rpoll == POLL_RESULT_TIMEOUT) {
+      continue;	
+    };
 
-				/// Read from socket.
-				char *buffer = malloc(1024);
-				socket_read(connection.fd, &buffer);				
-				
-				/// Write to socket.
-				for (int j = 0; j < data->nconnections; j++) {
-					if (j != i) {
-						socket_write(data->connections[j].fd, buffer);
-					};
-				};
-				free(buffer);
-			};
+    /// One or more connections have events available. 
+    if (rpoll == POLL_RESULT_SUCCESS) {
+      for (int i = 0; i < data->nconnections; i++) {
 
-			continue;
-		};	
-	};
-	
-	free(data->connections);
-	free(data);	
-	remove(SOCKET_PATH);
-	close(server);
-	exit(EXIT_SUCCESS);
+        struct pollfd connection = data->connections[i];
+
+        /// No events available.	
+        if (connection.revents == 0) {
+          continue;			
+        };
+
+        /// Probably, client connection terminated.
+        if (connection.revents != POLLIN) {					
+          /// Close connection.
+          close(connection.fd);
+
+          /// Create new connection array without closed connection.
+          int _nconnections = data->nconnections - 1;
+          for (int a = i; a < _nconnections; a++) {
+            data->connections[a] = data->connections[a+1];
+          };
+
+          /// Replace old connection array with resized one.
+          data->connections = realloc(data->connections, (_nconnections + 1) * sizeof(struct pollfd));
+          data->nconnections = _nconnections;
+
+          continue;
+        };
+
+        /// Read from socket.
+        char *buffer = malloc(1024);
+        socket_read(connection.fd, &buffer);				
+
+        /// Write to socket.
+        for (int j = 0; j < data->nconnections; j++) {
+          if (j != i) {
+            socket_write(data->connections[j].fd, buffer);
+          };
+        };
+        free(buffer);
+      };
+
+      continue;
+    };	
+  };
+
+  free(data->connections);
+  free(data);	
+  remove(SOCKET_PATH);
+  close(server);
+  exit(EXIT_SUCCESS);
 }
